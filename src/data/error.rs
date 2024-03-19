b7955482abdecdda8c6ae1e97cd2c5c600d8f84d
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-03-19 09:39:38 -0500

    Create PositionOpError enum with thiserror

--- src/data/error.rs
+++ src/data/error.rs
@@ -1,6 +1,31 @@
 use std::{error::Error, fmt::{Debug, Display, Formatter}};
 
+use thiserror::Error;
+
 use super::position::Position;
+
+#[derive(Error, Debug)]
+pub enum PositionOpError {
+    #[error("Playing at column {} would be out of bounds", .0 + 1)]
+    OutOfRangeCol(usize),
+
+    #[error("Column {} is full", .0 + 1)]
+    FullCol(usize),
+
+    #[error("Invalid sequence '{seq}': {source}, detected at index {bad_idx}")]
+    InvalidSequence {
+        #[source]
+        source: Box<PositionOpError>,
+        seq: String,
+        bad_idx: usize
+    },
+
+    #[error("Playing at column {} would prematurely solve position: \n{pos}", .col + 1)]
+    PrematureSolve {
+        col: usize,
+        pos: Position,
+    },
+}
 
 pub struct InvalidMoveError {
     col: usize,
