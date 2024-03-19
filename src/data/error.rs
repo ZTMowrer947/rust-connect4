a53e18ba4e673f2af3947091372bdf3938643882
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-03-19 09:43:56 -0500

    Remove previous error type

--- src/data/error.rs
+++ src/data/error.rs
@@ -1,5 +1,3 @@
-use std::{error::Error, fmt::{Debug, Display, Formatter}};
-
 use thiserror::Error;
 
 use super::position::Position;
@@ -26,29 +24,3 @@ pub enum PositionOpError {
         pos: Position,
     },
 }
-
-pub struct InvalidMoveError {
-    col: usize,
-    position: Position,
-}
-
-impl InvalidMoveError {
-    pub(crate) fn new(col: usize, pos: Position) -> Self {
-        Self { col, position: pos }
-    }
-}
-
-impl Debug for InvalidMoveError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "InvalidMoveError: invalidCol: {}", self.col)?;
-        write!(f, "\nPosition: {}", self.position)
-    }
-}
-
-impl Display for InvalidMoveError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Cannot perform move on column {} for this position:\n{}", self.col, self.position)
-    }
-}
-
-impl Error for InvalidMoveError {}
